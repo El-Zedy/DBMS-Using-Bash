ece9c89c30ba17c545a4315d113eabf641f749b9
El-Zedy <muhammadelzedy@fci.helwan.edu.eg>
2022-12-13 03:15:03 +0200

    finish createDB with some edit in master.sh

--- createDB.sh
+++ createDB.sh
@@ -1,4 +1,43 @@
 #!/bin/bash
 export LC_COLLATE=C # Terminal Case Sensitive
 shopt -s extglob #import Advanced Regex
-echo "create DB"
+
+
+read -p "|--Please Enter Database Name : " db_name
+
+case $db_name in
+
+    +([A-Za-z]))
+
+        if [ -d ./DBMS/$db_name ] ; then
+
+		    echo -e "
+        --------------------------------------
+        | Erorr! ($db_name) name is already exist.
+        --------------------------------------
+        "
+		    source	./createDB.sh
+		else
+
+            mkdir ./DBMS/$db_name
+            echo -e "
+        ----------------------------------------
+        | Database ($db_name) created successfully.
+        ----------------------------------------
+             "
+              
+
+		fi          
+    ;;
+    *)
+		echo "
+        -----------------------------
+        | Not Vaild Database name ! |
+        -----------------------------
+             "
+		source ./createDB.sh
+
+    ;;
+    
+esac
+
