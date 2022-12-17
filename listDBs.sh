8a0aa4eb938d1502b4abfb12b577becb9acae500
Mohamed Shaltout <mohamed.ahmed.shaltout@gmail.com>
2022-12-17 02:35:30 +0200

    edit in listDB.sh if dir DBMS empty

--- listDBs.sh
+++ listDBs.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 export LC_COLLATE=C             # Terminal Case Sensitive
 shopt -s extglob                #import Advanced Regex
-
-echo "
+if [ -z "$(ls -F ./DBMS | grep / )" ]; then         #check th DB lists if
+   echo "
         -----------------------
-        | Your Databases List: |
-        -----------------------"
-ls -F ./DBMS | grep /           #list bye type then grep only directories
-echo -e "\n"                    #newline
+        | NO DATABASES FOUND ! |
+        -----------------------"                #if there is no DB list it will return this message
+else
+    ls -F ./DBMS | grep /           #list bye type then grep only directories
+    echo -e "\n"                    #newline
+fi
