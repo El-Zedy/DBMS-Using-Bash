41fd8a156b092c19399621d189d9a4b02f9b8aae
mohamedshaltout58 <mohamed.ahmed.shaltout@gmail.com>
2022-12-26 11:22:27 +0200

    some updates in update table and select from table

--- selectFromTable.sh
+++ selectFromTable.sh
@@ -10,7 +10,7 @@ while [ true ]
 do
     if [ -f ./$tablename ] #if table exist
     then
-        select choice in "Select all Columns" "Select specific column" "Select from table with condition" "back to table menu" "back to main menu" Exit
+        select choice in "Select all Columns" "Select specific column" "back to table menu" "back to main menu" Exit
         do
             case $choice in
           
@@ -19,46 +19,9 @@ do
               ;;
               "Select specific column" )
                 
-                read -p "Please, Enter column name : " col_name                                  # to choose specific column 
+                read -p " ===> Please, Enter column name : " col_name                                  # to choose specific column 
                 col_num=`awk -F"|" -v col="$col_name" 'NR==1{for (i=1; i<=NF; i++) if ($i==col) {print i;exit}}' $tablename`  # col_num = number of field the column exist
                 awk -F"|" -v val="$col_num" '{print $val }' $tablename | column -t -s "|"  #print the choosen column
-              
-              
-              ;;
-              "Select from table with condition" )
-                while [ true ]
-                do
-
-                    read -p "Enter column name : " col_name
-                    col_num=`awk 'BEGIN{FS="|"}{if(NR==1){for (i=1;i<=NF;i++){if($i=="'$col_name'") print i}}}' $tablename`
-                    if [[ $col_num == "" ]]
-                    then
-                        echo "Wrong Column's Name !!!"
-                        continue
-                    else
-                        
-                        echo "Please choose the operator : [ ==, !=, >, <, >=, <= ] "
-                        read operator
-                        if [[ $operator == "==" ]] || [[ $operator == "!=" ]] || [[ $operator == ">" ]] || [[ $operator == "<" ]] || [[ $operator == ">=" ]] || [[ $operator == "<=" ]]
-                        then
-                            read -p "Enter the value : " value
-                            result=`awk 'BEGIN{FS"|"}{if (( $'$col_num$operator$value' )) print$0}' $tablename 2>> /dev/null | column -t -s "|"`
-                            if [[ result == "" ]]
-                            then
-                                echo "Value not found"
-                                continue
-                                                                                                
-                            else
-                                awk 'BEGIN{FS"|"}{if (( $'$col_num$operator$value' )) print$0}' $tablename 2>> /dev/null | column -t -s "|"
-                                break
-                            fi
-                        else
-                            echo "Wrong Input !"
-                            continue                            
-                        fi
-                    fi
-                done
-
               ;;
               "back to table menu" )
                 clear
